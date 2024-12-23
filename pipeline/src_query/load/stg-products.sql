37ac7fa68f5e72ebfea27002c8f7215b6c7973eb
hudiya.resa@gmail.com <hudiya.resa@gmail.com>
2024-12-23 13:06:35 +0700

    update join

--- pipeline/src_query/load/stg-products.sql
+++ pipeline/src_query/load/stg-products.sql
@@ -1,13 +1,15 @@
 MERGE INTO stg.products AS staging
 USING (
-    SELECT product_id, 
-    product_category_name, 
-    product_category_name_english, 
-    product_weight_g, 
-    product_length_cm, 
-    product_height_cm, 
-    product_width_cm
-    FROM public.products
+    SELECT p.product_id, 
+        p.product_category_name, 
+        pcat.product_category_name_english, 
+        p.product_weight_g, 
+        p.product_length_cm, 
+        p.product_height_cm, 
+        p.product_width_cm,
+    FROM public.products p
+    LEFT JOIN public.product_category_name_translation pcat
+        ON p.product_category_name = pcat.product_category_name
 ) source
 
 ON staging.product_id = source.product_id
@@ -15,12 +17,22 @@ ON staging.product_id = source.product_id
 WHEN MATCHED AND (
     staging.product_category_name <> source.product_category_name
     OR staging.product_category_name_english <> source.product_category_name_english
-
+    OR staging.product_weight_g <> source.product_weight_g
+    OR staging.product_length_cm <> source.product_length_cm
+    OR staging.product_height_cm <> source.product_height_cm
+    OR staging.product_width_cm <> source.product_width_cm
 ) THEN
-    UPDATE SET current_flag = "Expired", 
-    updated_at = CURRENT_TIMESTAMP
+    UPDATE SET 
+        staging.product_category_name = source.product_category_name,
+        staging.product_category_name_english = source.product_category_name_english,
+        staging.product_weight_g = source.product_weight_g,
+        staging.product_length_cm = source.product_length_cm,
+        staging.product_height_cm = source.product_height_cm,
+        staging.product_width_cm = source.product_width_cm,
+        staging.updated_at = CURRENT_TIMESTAMP
 
-WHEN NOT MATCHED BY SOURCE OR staging.current_flag = 'Expired' THEN
+-- When not matched, insert new record
+WHEN NOT MATCHED BY TARGET THEN
     INSERT (
         product_id, 
         product_category_name, 
@@ -30,8 +42,7 @@ WHEN NOT MATCHED BY SOURCE OR staging.current_flag = 'Expired' THEN
         product_height_cm, 
         product_width_cm,
         created_at, 
-        updated_at, 
-        current_flag
+        updated_at
     )
     VALUES (
         source.product_id, 
@@ -42,6 +53,5 @@ WHEN NOT MATCHED BY SOURCE OR staging.current_flag = 'Expired' THEN
         source.product_height_cm,
         source.product_width_cm, 
         CURRENT_TIMESTAMP, 
-        CURRENT_TIMESTAMP, 
-        "Current"
+        CURRENT_TIMESTAMP
     );
